c2b4c0756f6eb8c1da0d766d31b19bf47a690235
CarlosRivs <rivasangel.carlosantonio@utacapulco.edu.mx>
2023-02-08 13:13:55 -0600

    second commit

--- app/Views/User/Front/V_mapa.php
+++ app/Views/User/Front/V_mapa.php
@@ -1,4 +1,44 @@
-<?=$this->extend('User/Front/layout/main-Implan')?>
+<?=$this->extend('User/Front/layout/main')?>
+<!-- Services-->
+<?=$this->section('EstilosCss') ?>
+<link href="css/styles.css" rel="stylesheet" />
+<?=$this->endSection()?>
+
+<?=$this->section('menu')?>
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="<?php echo base_url()?>/"><img src="assets/img/im.png" alt="" /></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#services">¿QUIÉNES SOMOS?</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">NOTICIAS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">SOBRE NOSTROS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#team">EQUIPO</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">CONTACTO</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              PROYECTOS
+                            </a>
+                            <ul class="dropdown-menu">
+                            <li><hr class="dropdown-divider"></li>
+                              <li><a class="dropdown-item" href="<?php echo base_url()?>/bancoDP">Banco de Proyectos</a></li>
+                              <li><hr class="dropdown-divider"></li>
+                              <li><a class="dropdown-item" href="<?php echo base_url()?>/Acapulco">Acapulco</a></li>
+                              <li><hr class="dropdown-divider"></li>
+                              <li><a class="dropdown-item" href="<?php echo base_url()?>/mapa">Mapa</a></li>
+                            </ul>
+                          </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+<?=$this->endSection()?>
+
 <?=$this->section('css')?>
     <link href="./css/styles.css" rel="stylesheet"/>
     <style>
--- app/Views/User/Front/layout/main.php
+++ app/Views/User/Front/layout/main.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/styles.css" rel="stylesheet" />
+    <?=$this->renderSection('EstilosCss')?>
     <title><?= $this->renderSection('title')?></title>
     <?=$this->include('User/Front/layout/header')?>
     <?=$this->renderSection('css')?>
 </head>
 
-<?=$this->include('User/Front/layout/menu-IMPLAN') ?>
+<?=$this->renderSection('menu')?>
+
 <body id="page-top">
     <?=$this->renderSection('content')?>
     <?=$this->include('User/Front/layout/footer')?>
